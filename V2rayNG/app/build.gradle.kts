e21ede16f61fbae43820388193436999827b6dde
ggu4rd <contact@1vpn.org>
2025-12-18 23:51:19 -0500

    feat: Bump version to 2.1.2
    - Update Android Gradle Plugin to 8.12.3
    - Set versionName to "2.1.2" and versionCode to 298

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.v2ray.ang"
         minSdk = 23
         targetSdk = 35
-        versionCode = 297
-        versionName = "2.1.1"
+        versionCode = 298
+        versionName = "2.1.2"
         multiDexEnabled = true
 
         buildConfigField(
